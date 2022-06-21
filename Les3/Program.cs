64b355b3725174b79326bf444721dfba8e3dc281
XFX <sah0329@mail.ru>
2022-06-21 22:15:31 +0500

    Начало

---
+++ Les3/Program.cs
@@ -0,0 +1,4 @@
+﻿int numderA = 3;
+int numderB = 6;
+int result = numderA + numderB;
+Console.WriteLine(result);
---
+++ Les4/Program.cs
@@ -0,0 +1,3 @@
+﻿double numderA = 10;
+double numderB = 3;
+Console.WriteLine(numderA/numderB);
